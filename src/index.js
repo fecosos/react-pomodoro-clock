275f7b0f8c4252d3215c7a72452857b0ee6bd8de
Ruy Mallo <ruymallo@gmail.com>
2018-04-16 04:54:34 -0300

    Fixed webpack.config.prod

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'Styles/index.css';
 import Pomodoro from 'Components/Pomodoro/Pomodoro';
 import registerServiceWorker from './registerServiceWorker';
 
